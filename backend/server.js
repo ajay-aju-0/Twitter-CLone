8027c1589f74c474791771caad8ebd5b0535bfa6
ajayaju <ajaypadmanabhan01@gmail.com>
2024-06-29 15:30:42 +0530

    Prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
-import authRoutes from "./routes/authRoute.js"
-import userRoutes from "./routes/userRoute.js"
-import postRoutes from "./routes/postRoute.js"
-import notificationRoutes from "./routes/notificationRoute.js"
 import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+
+import authRoutes from "./routes/authRoute.js"
+import userRoutes from "./routes/userRoute.js"
+import postRoutes from "./routes/postRoute.js"
+import notificationRoutes from "./routes/notificationRoute.js"
 
 const app = express();
 dotenv.config();   // For reading the contents of env file
@@ -19,6 +21,8 @@ cloudinary.config({
 
 const PORT = process.env.PORT || 8080;
 
+const __dirname = path.resolve();
+
 app.use(express.json({limit:"5mb"})); // For parsing req.body
 app.use(express.urlencoded({extended:true})); // For parsing form data
 
@@ -28,6 +32,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notification",notificationRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 app.get('/',(req,res)=>{
     res.send("Server is ready")
